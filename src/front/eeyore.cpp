4647ed2aea88aad75eba8776ec54f543376ce571
Mai Huy Hoang <maihuyhoang97@gmail.com>
2020-12-09 09:31:07 +0700

    Add EeyoreType enum

--- src/front/eeyore.cpp
+++ src/front/eeyore.cpp
@@ -6,15 +6,21 @@ string EBase::to_string()
     return "EBase";
 }
 
+int EBase::get_type() { return E_BASE; }
+
 string EStmt::to_string()
 {
     return "EStmt";
 }
 
+int EStmt::get_type() { return E_STMT; }
+
 string EValue::to_string()
 {
     return "EValue";
 }
+
+int EValue::get_type() { return E_VALUE; }
 
 EVariable::EVariable(string name, bool is_temp) : name(name), is_temp(is_temp) {}
 
@@ -22,6 +28,8 @@ string EVariable::to_string()
 {
     return name;
 }
+
+int EVariable::get_type() { return E_VARIABLE; }
 
 ERightVal::ERightVal(EValue* name) : name(name)
 {
@@ -42,6 +50,8 @@ string ERightVal::to_string()
     }
 }
 
+int ERightVal::get_type() { return E_RIGHT_VAL; }
+
 ELeftVal::ELeftVal(EValue* name, EValue* index, bool is_array) : name(name), index(index), is_array(is_array) {}
 
 string ELeftVal::to_string()
@@ -53,6 +63,8 @@ string ELeftVal::to_string()
     }
 }
 
+int ELeftVal::get_type() { return E_LEFT_VAL; }
+
 EVarStmt::EVarStmt(EValue* name) : name(name) {}
 
 string EVarStmt::to_string()
@@ -60,12 +72,16 @@ string EVarStmt::to_string()
     return "var " + name->to_string();
 }
 
+int EVarStmt::get_type() { return E_VAR_STMT; }
+
 EVarArrayStmt::EVarArrayStmt(EValue* name, int size) : EVarStmt(name), size(size) {}
 
 string EVarArrayStmt::to_string()
 {
     return "var " + std::to_string(size) + " " + EVarStmt::name->to_string();
 }
+
+int EVarArrayStmt::get_type() { return E_VAR_ARRAY_STMT; }
 
 EAssignStmt::EAssignStmt(EValue* res, EValue* value) : res(res), value(value) {}
 
@@ -79,6 +95,8 @@ string EAssignStmt::to_string()
     return res->to_string() + " = " + value->to_string();
 }
 
+int EAssignStmt::get_type() { return E_ASSIGN_STMT; }
+
 EBinaryExpr::EBinaryExpr(EValue* res, EValue* lhs, int op, EValue* rhs) : res(res), lhs(lhs), op(op), rhs(rhs) {}
 
 string EBinaryExpr::to_string()
@@ -87,12 +105,16 @@ string EBinaryExpr::to_string()
            + " " + get_token_str(op) + " " + rhs->to_string();
 }
 
+int EBinaryExpr::get_type() { return E_BINARY_EXPR; }
+
 EUnaryExpr::EUnaryExpr(EValue* res, int op, EValue* rhs) : res(res), op(op), rhs(rhs) {}
 
 string EUnaryExpr::to_string()
 {
     return res->to_string() + " = " + get_token_str(op) + rhs->to_string();
 }
+
+int EUnaryExpr::get_type() { return E_UNARY_EXPR; }
 
 EParamStmt::EParamStmt(EValue* name) : name(name) {}
 
@@ -101,12 +123,16 @@ string EParamStmt::to_string()
     return "param " + name->to_string();
 }
 
+int EParamStmt::get_type() { return E_PARAM; }
+
 EFuncCall::EFuncCall(string func_name) : func_name(func_name) {}
 
 string EFuncCall::to_string()
 {
     return "call f_" + func_name;
 }
+
+int EFuncCall::get_type() { return E_FUNC_CALL; }
 
 EJumpLabel::EJumpLabel(string label) : label(label) {}
 
@@ -115,6 +141,8 @@ string EJumpLabel::to_string()
     return label + ":";
 }
 
+int EJumpLabel::get_type() { return E_LABEL; }
+
 EUnconditionalJump::EUnconditionalJump(string label) : label(label) {}
 
 string EUnconditionalJump::to_string()
@@ -122,12 +150,16 @@ string EUnconditionalJump::to_string()
     return "goto " + label;
 }
 
+int EUnconditionalJump::get_type() { return E_UNCOND_JUMP; }
+
 EConditionalJump::EConditionalJump(string label, EValue* cond_name, bool cond_value) : label(label), cond_name(cond_name), cond_value(cond_value) {}
 
 string EConditionalJump::to_string()
 {
     return "if " + cond_name->to_string() + (cond_value ? " != 0" : " == 0") + " goto " + label;
 }
+
+int EConditionalJump::get_type() { return E_COND_JUMP; }
 
 EReturnStmt::EReturnStmt(EValue* value) : value(value) {}
 
@@ -139,6 +171,8 @@ string EReturnStmt::to_string()
         return "return";
 }
 
+int EReturnStmt::get_type() { return E_RETURN; }
+
 EFuncDef::EFuncDef(string func_name, int args_num) : func_name(func_name), args_num(args_num) {}
 
 string EFuncDef::to_string()
@@ -146,9 +180,13 @@ string EFuncDef::to_string()
     return "f_" + func_name + " [ " + std::to_string(args_num) + " ]";
 }
 
+int EFuncDef::get_type() { return E_FUNC_DEF; }
+
 EFuncEnd::EFuncEnd(string func_name) : func_name(func_name) {}
 
 string EFuncEnd::to_string()
 {
     return "end f_" + func_name;
 }
+
+int EFuncEnd::get_type() { return E_FUNC_END; }
--- src/front/eeyore.hpp
+++ src/front/eeyore.hpp
@@ -1,22 +1,48 @@
 #pragma once
 #include "globals.hpp"
 
+enum EeyoreType
+{
+    E_BASE,
+    E_STMT,
+    E_VALUE,
+    E_VARIABLE,
+    E_RIGHT_VAL,
+    E_LEFT_VAL,
+    E_VAR_STMT,
+    E_VAR_ARRAY_STMT,
+    E_ASSIGN_STMT,
+    E_BINARY_EXPR,
+    E_UNARY_EXPR,
+    E_PARAM,
+    E_FUNC_CALL,
+    E_FUNC_DEF,
+    E_FUNC_END,
+    E_UNCOND_JUMP,
+    E_COND_JUMP,
+    E_RETURN,
+    E_LABEL
+};
+
 class EBase
 {
 public:
     virtual string to_string();
+    virtual int get_type();
 };
 
 class EStmt : public EBase
 {
 public:
     virtual string to_string();
+    virtual int get_type();
 };
 
 class EValue : public EStmt
 {
 public:
     virtual string to_string();
+    virtual int get_type();
 };
 
 class EVariable : public EValue
@@ -26,6 +52,7 @@ public:
     bool is_temp;
     EVariable(string name, bool is_temp=true);
     virtual string to_string();
+    virtual int get_type();
 };
 
 class ERightVal : public EValue
@@ -37,6 +64,7 @@ public:
     ERightVal(EValue* name);
     ERightVal(int value);
     virtual string to_string();
+    virtual int get_type();
 };
 
 class ELeftVal : public EValue 
@@ -47,6 +75,7 @@ public:
     EValue* index;
     ELeftVal(EValue* name, EValue* index=nullptr, bool is_array=false);
     virtual string to_string();
+    virtual int get_type();
 };
 
 class EVarStmt : public EStmt
@@ -55,15 +84,16 @@ public:
     EValue* name;
     EVarStmt(EValue* name);
     virtual string to_string();
+    virtual int get_type();
 };
 
 class EVarArrayStmt : public EVarStmt 
 {
 public:
-    EValue* name;
     int size;
     EVarArrayStmt(EValue* name, int size);
     virtual string to_string();
+    virtual int get_type();
 };
 
 class EAssignStmt : public EStmt
@@ -74,6 +104,7 @@ public:
     EAssignStmt(EValue* res, int value);
     EAssignStmt(EValue* res, EValue* value);
     virtual string to_string();
+    virtual int get_type();
 };
 
 class EBinaryExpr : public EStmt
@@ -85,6 +116,7 @@ public:
     EValue* rhs;
     EBinaryExpr(EValue* res, EValue* lhs, int op, EValue* rhs);
     virtual string to_string();
+    virtual int get_type();
 };
 
 class EUnaryExpr : public EStmt 
@@ -95,6 +127,7 @@ public:
     EValue* rhs;
     EUnaryExpr(EValue* res, int op, EValue* rhs);
     virtual string to_string();
+    virtual int get_type();
 };
 
 class EParamStmt : public EStmt 
@@ -103,6 +136,7 @@ public:
     EValue* name;
     EParamStmt(EValue* name);
     virtual string to_string();
+    virtual int get_type();
 };
 
 class EFuncCall : public EValue 
@@ -111,6 +145,7 @@ public:
     string func_name;
     EFuncCall(string func_name);
     virtual string to_string();
+    virtual int get_type();
 };
 
 class EJumpLabel : public EStmt 
@@ -119,6 +154,7 @@ public:
     string label;
     EJumpLabel(string label);
     virtual string to_string();
+    virtual int get_type();
 };
 
 class EUnconditionalJump : public EStmt 
@@ -127,6 +163,7 @@ public:
     string label;
     EUnconditionalJump(string label);
     virtual string to_string();
+    virtual int get_type();
 };
 
 class EConditionalJump : public EStmt 
@@ -137,6 +174,7 @@ public:
     bool cond_value;
     EConditionalJump(string label, EValue* cond_name, bool cond_value);
     virtual string to_string();
+    virtual int get_type();
 };
 
 class EReturnStmt : public EStmt
@@ -145,6 +183,7 @@ public:
     EValue* value;
     EReturnStmt(EValue* value=nullptr);
     virtual string to_string();
+    virtual int get_type();
 };
 
 class EFuncDef : public EStmt 
@@ -154,6 +193,7 @@ public:
     int args_num;
     EFuncDef(string func_name, int args_num);
     virtual string to_string();
+    virtual int get_type();
 };
 
 class EFuncEnd : public EStmt 
@@ -162,5 +202,6 @@ public:
     string func_name;
     EFuncEnd(string func_name);
     virtual string to_string();
+    virtual int get_type();
 };
 
